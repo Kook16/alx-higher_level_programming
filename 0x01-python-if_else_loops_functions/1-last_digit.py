20f89d0ea2dee0a5874d0a73ca3ec7d42264d313
Kook16 <jamess075@gmail.com>
2023-06-06 20:23:11 +0000

    Fixed the issue of last digit of a negative number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,11 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
-last_digit = number % 10
+if number < 0:
+    unsigned_number = number * -1
+else:
+    unsigned_number = number
+last_digit = unsigned_number % 10
 if last_digit > 5:
     message = 'and is greater than 5'
 elif last_digit < 6 and last_digit > 0:
