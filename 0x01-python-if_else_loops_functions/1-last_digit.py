c967224f6a543f886219b33165d54e1dcf918f79
Kook16 <jamess075@gmail.com>
2023-06-06 20:32:45 +0000

    Fixed the if statement on to check for the right condition on file  1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,13 +3,14 @@ import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
 if number < 0:
-    unsigned_number = number * -1
+    signed_number = number * -1
+    last_digit = -1 * (signed_number % 10)
 else:
     unsigned_number = number
-last_digit = unsigned_number % 10
+    last_digit = unsigned_number % 10
 if last_digit > 5:
     message = 'and is greater than 5'
-elif last_digit < 6 and last_digit > 0:
+elif last_digit < 6 and not 0:
     message = 'and is less than 6 and not 0'
 else:
     message = 'and is 0'
