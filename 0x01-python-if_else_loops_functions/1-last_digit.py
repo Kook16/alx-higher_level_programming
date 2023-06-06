4c81e17c89978d147fb2c9c67f6ab95f67812441
Kook16 <jamess075@gmail.com>
2023-06-06 20:38:55 +0000

    fixed the error on the condition statment in file 1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,8 +10,8 @@ else:
     last_digit = unsigned_number % 10
 if last_digit > 5:
     message = 'and is greater than 5'
-elif last_digit < 6 and not 0:
+elif last_digit < 6 and last_digit != 0:
     message = 'and is less than 6 and not 0'
-else:
+elif last_digit == 0:
     message = 'and is 0'
 print(f"Last digit of {number} is {last_digit} {message}")
