dfeed956daf90f458df9066f81a2f98f0cd477d4
Kook16 <jamess075@gmail.com>
2023-06-10 15:57:34 +0000

    added a check to see if the list is empty on 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,9 @@
 
 def new_in_list(my_list, idx, element):
     my_list_copy = my_list[:]
-    if my_list is None or idx > len(my_list) -1:
+    if my_list is None:
+        return
+    elif idx < 0 or idx > len(my_list) -1:
         return my_list_copy
     else:
         my_list_copy[idx] = element
