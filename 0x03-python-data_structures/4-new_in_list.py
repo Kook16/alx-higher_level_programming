893703f01d452361545f2b06123fc89bcc1caf1d
Kook16 <jamess075@gmail.com>
2023-06-10 15:53:25 +0000

    fixed an error

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,9 +2,8 @@
 
 def new_in_list(my_list, idx, element):
     my_list_copy = my_list[:]
-    if idx < 0 or idx > len(my_list) -1:
+    if my_list is None or idx > len(my_list) -1:
         return my_list_copy
     else:
         my_list_copy[idx] = element
         return my_list_copy
-
