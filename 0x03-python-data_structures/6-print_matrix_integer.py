91a67e2f9b98800105186f0fe52053770948d66f
Kook16 <jamess075@gmail.com>
2023-06-10 16:23:09 +0000

    fixed the formatting style on 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        return
     for item in matrix:
+        i = 0
         for item1 in item:
-            print('{} '.format(item1), end='')
+            if i != len(item) -1:
+                print('{:d} '.format(item1), end='')
+                i += 1
+            else:
+                print('{:d}'.format(item1), end='')
         print()
-
-       
