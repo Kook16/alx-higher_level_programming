2a02895dd56796c8e40a69effe95c04eaa7a39b6
Kook16 <jamess075@gmail.com>
2023-06-10 12:22:36 +0000

    added new project files

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for item in matrix:
+        for item1 in item:
+            print('{}'.format(item1), end=' ')
+        print()
+
+       
