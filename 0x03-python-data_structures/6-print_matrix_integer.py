86465503a78fc374fb9da1243be860f71b660287
Kook16 <jamess075@gmail.com>
2023-06-12 08:19:22 +0000

    fixed pycodestyle errors

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
     if matrix is None:
         return
     for item in matrix:
         i = 0
         for item1 in item:
-            if i != len(item) -1:
+            if i != len(item) - 1:
                 print('{:d} '.format(item1), end='')
-                i += 1
             else:
                 print('{:d}'.format(item1), end='')
+            i += 1
         print()
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-
 def max_integer(my_list=[]):
-    if my_list is None:
-        return
-    max_int = my_list[0]
     if len(my_list) == 0:
         return None
+    max_int = my_list[0]
     for item in my_list:
         if item > max_int:
             max_int = item
