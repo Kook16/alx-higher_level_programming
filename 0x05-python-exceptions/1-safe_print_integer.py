40cb9ee7518cc6674c83805b6ea48c9734211c3d
Kook16 <jamess075@gmail.com>
2023-06-26 19:44:07 +0000

    Added int() to print only integers

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@ def safe_print_integer(value):
     if value is None:
         return
     try:
-        print("{:d}".format(value))
+        print("{:d}".format(int(value)))
     except (ValueError, TypeError):
         return False
     return True
