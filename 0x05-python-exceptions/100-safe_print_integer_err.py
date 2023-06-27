808d261444ae492c938efe4edd036b44bb82fd81
Kook16 <jamess075@gmail.com>
2023-06-27 00:14:54 +0000

    completed task 4

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++
@@ -1,8 +0,0 @@
-def safe_print_integer_err(value):
-    import sys
-    try:
-        print("{:d}".format(value))
-    except (ValueError, TypeError) as e:
-        print("Exception: {}".format(e), file=sys.stderr)
-        return False
-    return True
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    if my_list_1 is None or my_list_2 is None:
-        print('out of range')
-    result = 0
     list = []
-    i = 0
-    while i < list_length:
-        try:
-            result = my_list_1[i] / my_list_2[i]
-        except TypeError:
-            print("wrong type")
-            return
-        except ZeroDivisionError:
-            print('division by 0')
-            return
-        except IndexError:
-            print("out of range")
-            return
-        finally:
-            list.append(result)
-        i += 1
-    return list
+    try:
+        for i in range(list_length):
+            try:
+                list.append(my_list_1[i] / my_list_2[i])
+            except TypeError:
+                list.append(0)
+                print("wrong type")
+            except ZeroDivisionError:
+                list.append(0)
+                print('division by 0')
+            except IndexError:
+                list.append(0)
+                print("out of range")
+
+    finally:
+        return list
