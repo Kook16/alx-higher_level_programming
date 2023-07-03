81afa2563f8351100866c2af7d522e04b0256add
Kook16 <jamess075@gmail.com>
2023-07-03 18:56:37 +0000

    Updated task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
         '''
         initiailization function
         '''
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
