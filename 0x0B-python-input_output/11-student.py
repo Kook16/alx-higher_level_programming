30d4ff97b90fc2216a680f6efe450cc30527c3d5
Kook16 <jamesss075@gmail.com>
2023-07-11 20:15:18 +0000

    Added task 10 and 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''Defining a Student class
+'''
+
+
+class Student:
+    '''Student class defines a student
+    '''
+    def __init__(self, first_name, last_name, age):
+        '''This is the intializer function'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''this function retrieves a dict representation of class instance'''
+        if type(attrs) is list and all(type(ele) is str for ele in attrs):
+            json_data = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    json_data[attr] = getattr(self, attr)
+            return json_data
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        '''that replaces all attributes of the Student instance'''
+        for key, value in json.items():
+            setattr(self, key, value)
