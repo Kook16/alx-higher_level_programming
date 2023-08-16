3dc5ce5e855c5a232add2630a9380e28b37a6858
Kook16 <jamesss075@gmail.com>
2023-08-16 23:31:41 +0300

    Completed the manadatory tasks

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,11 @@
+-- a script that lists all Comedy shows in the database hbtn_0d_tvshows.
+
+-- The tv_genres table contains only one record where name = Comedy (but the id can be different)
+-- Each record should display: tv_shows.title
+-- Results must be sorted in ascending order by the show title
+-- You can use only one SELECT statement
+
+SELECT tv.`title` FROM `tv_shows` AS tv
+INNER JOIN `tv_show_genres` AS show ON tv.`id` = show.`show_id`
+INNER JOIN `tv_genres` AS gen ON gen.`id` = show.`genre_id`
+WHERE gen.`name` = "Comedy" ORDER BY tv.`title`;
---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+-- If a show doesn’t have a genre, display NULL in the genre column
+-- Each record should display: tv_shows.title - tv_genres.name
+-- Results must be sorted in ascending order by the show title and genre name
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+SELECT tv.`title`, gen.`name` FROM `tv_shows` AS tv
+LEFT JOIN `tv_show_genres` AS show ON tv.`id` = show.`show_id`
+LEFT JOIN `tv_genres` AS gen ON show.`genre_id` = gen.`id`
+ORDER BY tv.`title`, gen.`name`;
