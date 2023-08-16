1f2c78d83e9935b190463a04cffcfde7671eb2fa
Kook16 <jamesss075@gmail.com>
2023-08-16 23:39:43 +0300

    fixed a typo on task 9

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,8 +2,6 @@
 
 -- The states table contains only one record where name = California (but the id can be different, as per the example)
 -- Results must be sorted in ascending order by cities.id
-
-USE hbtn_0d_usa;
-SELECT cities.id FROM cities
+SELECT cities.id, name FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name='California')
 ORDER BY cities.i ASC;
