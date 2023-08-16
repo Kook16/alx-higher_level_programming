a1e61eb7fde1504a8b15148b6c6881d465bf65f1
Kook16 <jamesss075@gmail.com>
2023-08-17 00:14:33 +0300

    Updated task 15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -5,7 +5,12 @@
 -- Results must be sorted in ascending order by the show title
 -- You can use only one SELECT statement
 
-SELECT tv.`title` FROM `tv_shows` AS tv
-INNER JOIN `tv_show_genres` AS show ON tv.`id` = show.`show_id`
-INNER JOIN `tv_genres` AS gen ON gen.`id` = show.`genre_id`
-WHERE gen.`name` = "Comedy" ORDER BY tv.`title`;
+SELECT tv.`title`
+  FROM `tv_shows` AS tv
+       INNER JOIN `tv_show_genres` AS show
+       ON tv.`id` = show.`show_id`
+
+       INNER JOIN `tv_genres` AS gen
+       ON gen.`id` = show.`genre_id`
+       WHERE gen.`name` = "Comedy"
+ ORDER BY tv.`title`;
