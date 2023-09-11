cdf37f2ea9586706245a18dd6b37e90b05aa4851
Kook16 <jamesss075@gmail.com>
2023-09-11 18:58:59 +0300

    corrected task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,9 +4,8 @@
 // if the first argument can be converted to an integer:
 
 const value = parseInt(process.argv[2]);
-let message;
 
-if (value === NaN) { console.log('Not a number'); } else { console.log(`My number: ${value}`); }
+if (isNaN(value)) { console.log('Not a number'); } else { console.log(`My number: ${value}`); }
 
 process.argv.forEach((val, index) => {
   console.log(`${index} ${val}`);
