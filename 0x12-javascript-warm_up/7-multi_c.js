903ec1c6f714a30a726fffe8f4acc0189db7a746
Kook16 <jamesss075@gmail.com>
2023-09-11 19:10:44 +0300

    fixed requirement errors on task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-
-// script that prints x times “C is fun”
-
 const times = process.argv[2];
 
-if (!times) { console.log('Missing number of occurrences'); }
-else if (isNaN(time) && times >= 0) {
+if (!times) { console.log('Missing number of occurrences'); } else if (!isNaN(times) && times > 0) {
   let i = 0;
   while (i < times) {
     console.log('C is fin');
