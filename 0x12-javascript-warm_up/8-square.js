c46a9f92e082214382ee50289e1855bcfb8dc675
Kook16 <jamesss075@gmail.com>
2023-09-11 19:34:52 +0300

    corrected a requirement check on task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,10 +4,12 @@
 
 const size = process.argv[2];
 
-if (!size) { console.log('Missing size'); } else if (!isNaN(size)) {
+if (!isNaN(size)) {
   let i = 0;
   while (i < size) {
     console.log('X'.repeat(size));
     i++;
   }
+} else {
+    console.log('Missing size');
 }
