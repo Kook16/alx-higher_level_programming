40f583bb0c6b6615d49f82319a02dbe044537326
Kook16 <jamesss075@gmail.com>
2023-09-11 18:07:56 +0300

    corrected requirement check task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const length = process.argv.length;
-if (length <= 2) {
+if (length == 2) {
     console.log( 'No argument');
 } else if (length == 3) {
     console.log('Argument found');
