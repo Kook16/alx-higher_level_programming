5112a15b5cc665ea2deae95d6b7811a248c65129
Kook <123327622+Kook16@users.noreply.github.com>
2023-09-15 22:17:31 +0300

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,4 +3,4 @@
 -- The user_0d_1 password should be set to user_0d_1_pwd
 -- If the user user_0d_1 already exists, your script should not fail
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' INDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILIGES ON * . * TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILIGES ON *.* TO 'user_0d_1'@'localhost';
