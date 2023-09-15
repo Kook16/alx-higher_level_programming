7ace669633efc9a0ab1e57e89e531a8f1f1402ec
Kook16 <jamesss075@gmail.com>
2023-09-15 17:46:40 +0300

    fixed a typo in task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-'''
-script that lists all states from the database hbtn_0e_0_usa
-'''
-import sys
+"""a script that lists all states from the database hbtn_0e_0_usa"""
+
 import MySQLdb
-
-
+import sys
 if __name__ == "__main__":
-    # connecting to the database
     db = MySQLdb.connect(
-        host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
 
-    cur =  db.cursor()
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states;"
 
-    cur.execute("SELECT * FROM states;")
-
-    results = cur.fetchall()
-
-    for row in results:
-        print(row)
-
+    )
+    states = cur.fetchall()
+    for state in states:
+        print(state)
     cur.close()
     db.close()
