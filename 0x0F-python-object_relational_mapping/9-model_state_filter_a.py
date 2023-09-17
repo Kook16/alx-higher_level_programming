eadcb118e72e33818115227abf22f1a006cfd431
Kook16 <jamesss075@gmail.com>
2023-09-17 13:48:48 -0700

    fixed pycostyle for task 9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,12 +12,13 @@ if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(username, password, database),
                            pool_pre_ping=True)
-    
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
-    
+    states_with_a = session.query(State).filter(State.name.like('%a%')).\
+        order_by(State.id).all()
+
     for state in states_with_a:
         print("{}: {}".format(state.id, state.name))
 
