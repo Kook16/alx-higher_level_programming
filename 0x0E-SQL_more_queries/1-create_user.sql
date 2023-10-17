8161fddc53e09e17d93f94b2b8f392a38f6628a1
kook16 <jamesss075@gmail.com>
2023-10-17 09:59:42 +0300

    removed a dir

--- 0x0E-SQL_more_queries/1-create_user.sql
+++
@@ -1,3 +0,0 @@
--- If the user user_0d_1 already exists, your script should not fail
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' INDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILIGES ON *.* TO 'user_0d_1'@'localhost';
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++
@@ -1,7 +0,0 @@
--- a script that lists all cities contained in the database hbtn_0d_usa.
--- Each record should display: cities.id - cities.name - states.name
--- Results must be sorted in ascending order by cities.id
--- You can use only one SELECT statement
-SELECT cities.id, cities.name, states.name FROM cities
-JOIN states ON cities.state_id = state.id
-ORDER BY cities.id;
