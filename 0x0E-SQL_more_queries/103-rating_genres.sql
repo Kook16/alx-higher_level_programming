d5a3848c9c2c0a64c57fed3a33cb447a4781774c
kook16 <jamesss075@gmail.com>
2023-10-19 01:42:11 +0300

    updated task 103

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,8 +1,11 @@
 -- a script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
-SELECT tv.title, gen.name
- FROM tv_shows AS tv
-       LEFT JOIN tv_show_genres AS show
-       ON tv.id = show.show_id
-       LEFT JOIN tv_genres AS gen
-       ON show.genre_id = gen.id
- ORDER BY tv.title, gen.name DESC;
+SELECT name, SUM(rate) AS rating
+  FROM tv_genres AS g
+       INNER JOIN tv_show_genres AS s
+       ON s.genre_id = g.id
+
+       INNER JOIN tv_show_ratings AS r
+       ON r.show_id = s.show_id
+ GROUP BY name
+ ORDER BY rating DESC;
+ 
