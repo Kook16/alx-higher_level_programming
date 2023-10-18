382c9c9a76a89b0b1bad42677287d0d5e76e1739
kook16 <jamesss075@gmail.com>
2023-10-19 00:02:22 +0300

    updated task 101

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -16,5 +16,7 @@ WHERE tv_genres.name NOT IN(
         ON tv_shows.id = tv_show_genres.show_id
     WHERE tv_shows.title = 'Comedy'
 )
-ORDER BY tv_shows.title
+ORDER BY
+    tv_shows.title,
+    tv_genres.name
 ;
