96678dab5245cf3f51dcbbe142c19d36c75ccb2a
kook16 <jamesss075@gmail.com>
2023-10-19 00:09:43 +0300

    updated task 101

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -3,9 +3,9 @@ SELECT
     DISTINCT
         tv_shows.title
 FROM tv_shows
-JOIN tv_show_genres
+LEFT JOIN tv_show_genres
     ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres
+LEFT JOIN tv_genres
     ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.name NOT IN(
     SELECT tv_genres.name
@@ -17,6 +17,4 @@ WHERE tv_genres.name NOT IN(
     WHERE tv_shows.title = 'Comedy'
 )
 ORDER BY
-    tv_shows.title,
-    tv_genres.name
-;
+    tv_shows.title;
