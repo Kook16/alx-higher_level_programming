622a705bbe94bde876be1b0be963b602dfb497a0
Kook16 <jamesss075@gmail.com>
2023-11-30 14:23:57 -0800

    updates

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-#  a Bash script that takes in a URL, sends a GET request to the URL
-# and displays the body of the response
-
-curl -sN "$1"
+# script that takes in a URL, sends a GET request to the URL and displays the body of the response
+curl -sL "$1"
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# a Bash script that takes in a URL as an argument sends a GET request to the URL, and displays the body of the response
+# script that takes in URL as an argument sends a GET request to the URL, and displays the body of the response
 curl -sH "X-School-User-Id: 89" "$1"
